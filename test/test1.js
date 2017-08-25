5cea8731e198334cbd496d8999e9390aa69b2e0e
Chen <chiyanc@amazon.com>
2017-08-24 19:23:03 -0700

    * Wraping up...

--- test/test1.js
+++ test/test1.js
@@ -1,15 +1,52 @@
+var sync = require('node-sync').sync3;
+var co = sync.proc;
+var $let = sync.letImplicit;
+var $get = sync.implicit;
+var lift = sync.lift;
+
+var $U = require('underscore');
+
+var qoop = require('qoop');
+
 var DBCfg = require('../lib/db_config');
 
-var main = co(function*() {
+var cfg = {
+
+};
+
+var $M;
+var prepare = co(function*() {
     var dbCfg = new DBCfg(cfg);
 
     yield dbCfg.ready();
     var db = dbCfg.getDB({
         'connectionLimit': 1
     });
-    var $M = dbCfg.getModel;
+    $M = dbCfg.getModel;
+    return db;
+});
 
-    db.context(co(function*() {
-        yield $M('users').create(dto);
-    }));
+var run = co(function*() {
+	var cb = yield prepare();
+
+    yield db.context(yield main());
+});
+
+var main = co(function*() {
+	var dto = {
+
+	};
+
+	yield $M('users').create(dto);
+});
+
+run()({}, {}, function(s, err, res) {
+	if (err) {
+		console.log('Error:', err);
+		if (err.stack) {
+			console.log(err.stack);
+		}
+	} else {
+		console.log('Ok:', res);
+	}
 });
