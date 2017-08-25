5c00e88f150dc5990c96b8467abdc64f505d69de
redblaze <redblaze2005@gmail.com>
2017-08-25 10:13:14 -0700

    * Bug fixes

--- test/test1.js
+++ test/test1.js
@@ -11,34 +11,86 @@ var qoop = require('qoop');
 var DBCfg = require('../lib/db_config');
 
 var cfg = {
-
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'reporter'
 };
 
 var $M;
 var prepare = co(function*() {
-    var dbCfg = new DBCfg(cfg);
+    var dbCfg = new DBCfg({
+        'createdFieldName': 'creationdate',
+        'updatedFieldName': 'modificationdate',
+        'supportSoftDelete': false,
+        'supportOptimisticLock': false,
+        'connection': cfg
+    });
 
     yield dbCfg.ready();
     var db = dbCfg.getDB({
         'connectionLimit': 1
     });
-    $M = dbCfg.getModel;
+    $M = function(name) {return dbCfg.getModel(name);};
     return db;
 });
 
 var run = co(function*() {
-	var cb = yield prepare();
-
-    yield db.context(yield main());
+    try {
+        var db = yield prepare();
+        return (yield db.context(main));
+    } finally {
+        db.end();
+    }
 });
 
 var main = co(function*() {
-	var dto = {
-
-	};
-
-	yield $M('users').create(dto);
+    return yield txn();
 });
+
+var createUser = co(function*() {
+    var newUser = yield $M('User').create({
+        'email': 'foo@bar.com',
+        'handle': 'foobar',
+        'phone': '12345'
+    });
+
+    return newUser;
+});
+
+var updateUser = co(function*() {
+    return yield $M('User').update({'id': 8}, {
+        'phone': '23456'
+    });
+});
+
+var removeUser = co(function*() {
+    return yield $M('User').remove(9);
+});
+
+var sleep = co(function*(time) {
+    yield function(e, s, cb) {
+        setTimeout(function() {
+            cb(s);
+        }, time);
+    };
+});
+
+var txn = co(function*(time, s) {
+    var User = $M('User');
+
+    yield DBCfg.transaction(co(function*() {
+        var row = yield User.lockById(8);
+        yield sleep(time);
+        if (row) {
+            yield User.update(row, {
+                'email': row['email'] + s
+            });
+        }
+    }));
+});
+
+
 
 run()({}, {}, function(s, err, res) {
 	if (err) {
